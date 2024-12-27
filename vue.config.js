e539a47e7b1c3a988683e3a34aadea6d333b0714
Neck <edunepta@gmail.com>
2024-12-27 00:32:49 -0500

    Implementación de Vutify

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
@@ -11,5 +12,11 @@ module.exports = defineConfig({
         '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false)
       })
     ]
+  },
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
   }
 });
